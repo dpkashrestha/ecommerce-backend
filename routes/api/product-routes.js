b375d126055651a7fa6a103a0eb40671b22926fc
dpkashrestha <dpkashrestha7@gmail.com>
2023-12-07 14:14:57 -0600

    Completed all model routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,45 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [
+        {model: Category},
+        {model: Tag, as: 'product_tags',through: ProductTag,}
+      ],
+    });
+
+    res.json(productData);
+  } catch (err) {
+    console.error('Error fetching products:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        {model: Category},
+        {model: Tag, as: 'product_tags',through: ProductTag,}
+        ],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No Product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -92,8 +122,26 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+    try {
+      const productData = await Product.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!productData) {
+        res.status(404).json({ message: 'No Product found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(productData);
+    } catch (err) {
+      console.error('Error deleting product:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
+
 
 module.exports = router;
